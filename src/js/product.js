18e71bfa6ef1ffb4399eeeb5153713b143447d7b
MBarker2BYU <mbarker2@byupathway.edu>
2025-07-10 09:36:37 -0400

    Partial Commit of Team Assignment

--- src/js/product.js
+++ src/js/product.js
@@ -1,31 +1,12 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from './ProductDetails.mjs';
 
-const dataSource = new ProductData("tents");
+const product = 'product';
+const tents = 'tents';
 
-function addProductToCart(product) {
-  // retrieve existing cart from local storage
-  let cart = getLocalStorage("so-cart") || [];
+const productId = getParam(product);
+const dataSource = new ProductData(tents);
+const productDetails = new ProductDetails(productId, dataSource);
 
-  // if cart is not an array, convert it to an array
-  if (!Array.isArray(cart)) {
-    cart = [cart];
-  }
-
-  // Add the new product to the cart array
-  cart.push(product);
-
-  // save updated cart to local storage
-  setLocalStorage("so-cart", cart);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+productDetails.init();
