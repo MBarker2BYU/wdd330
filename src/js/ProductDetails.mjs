70e2d5271a43306c44d11e2c855185a4754a5313
MBarker2BYU <mbarker2@byupathway.edu>
2025-07-10 10:01:57 -0400

    Team 7 week 2 assignment

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,84 @@
+import { setLocalStorage } from './utils.mjs';
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  async init() {
+    try {
+      this.product = await this.dataSource.findProductById(this.productId);
+      
+      if (!this.product) {
+        throw new Error('Product not found');
+      }
+
+      this.renderProductDetails();
+
+      const addToCartButton = document.getElementById('addToCart');
+      
+      if (addToCartButton) {
+        addToCartButton.addEventListener('click', this.addProductToCart.bind(this));
+      } 
+      else {
+        console.error('Add to Cart button not found');
+      }
+
+    } catch (error) {
+      console.error('Error initializing product details:', error);
+      document.querySelector('main').innerHTML = '<p>Error loading product details</p>';
+    }
+}
+
+  addProductToCart() {
+  try {
+
+    let cart = JSON.parse(localStorage.getItem('so-cart')) || [];
+
+    if (!Array.isArray(cart)) {
+      cart = [cart].filter(item => item); //Remove any null values.
+    }
+    
+    cart.push(this.product);
+    
+    setLocalStorage('so-cart', cart);
+    
+    console.log('Cart updated:', cart);
+    
+  } catch (error) {
+    console.error('Error adding to cart:', error);
+  }
+}
+
+  renderProductDetails() {
+    // Select the main container for product details
+    const mainElement = document.querySelector('main');
+
+    // Check if product details are available
+    if(!this.product || !this.product.Brand || !this.product.Colors) {
+      mainElement.innerHTML = '<p>Product details are not available</p>';
+      return;
+    }
+
+    // Generate HTML for product details using product data
+    mainElement.innerHTML = `
+      <section class="product-detail">
+        <h3>${this.product.Brand.Name}</h3>
+        <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+        <img
+          class="divider"
+          src="${this.product.Image}"
+          alt="${this.product.NameWithoutBrand}"
+        />
+        <p class="product-card__price">$${this.product.ListPrice}</p>
+        <p class="product__color">${this.product.Colors[0].ColorName}</p>
+        <p class="product__description">${this.product.DescriptionHtmlSimple}</p>
+        <div class="product-detail__add">
+          <button id="addToCart" data-id="${this.productId}">Add to Cart</button>
+        </div>
+      </section>
+    `;
+  }
+}
